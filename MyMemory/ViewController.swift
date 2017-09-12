6fb0497177e46f2c4bfe52a0aaa27b58dc255620
imHyang taek s <spprid@simHyangui-MacBook-Pro.local>
2017-09-13 00:25:42 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Spprid on 2017. 9. 13..
+//  Copyright © 2017년 Spprid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
